0e68c30899a2c3536e1ea4500e5f3ab9fec62a0d
zhongzihuan <757537413@qq.com>
2019-02-21 14:36:40 +0800

    demo更新

--- demo/complex-scene/src/modules/demo/ModuleRouter.js
+++ demo/complex-scene/src/modules/demo/ModuleRouter.js
@@ -1,26 +1,29 @@
-import React from 'react';
+/**
+ * 模块路由
+ */
+import React, { Component } from 'react';
 import { Route } from 'mango-web/router';
 import { MangoUtils } from 'mango-web';
 import ToNamePage from './pages/toName/ToNamePage';
 import ToNameModel from './pages/toName/ToNameModel';
 
-const ModuleRouter = (props) => {
+export default class ModuleRouter extends Component {
 
-	const {match, app} = props;
+	render() {
+		const {match, app} = this.props;
+		return (
+			<div>
+				<Route
+					exact
+					path={`${match.path + '/ToNamePage'}`}
+					component={ToNamePage}
+					registerModel={MangoUtils.registerModel(app, ToNameModel)}
+				/>
+			</div>
+		);
+	}
+}
 
-	return (
-		<div>
-			<Route
-				exact
-				path={`${match.path + '/ToNamePage'}`}
-				component={ToNamePage}
-				registerModel={MangoUtils.registerModel(app, ToNameModel)}
-			/>
-		</div>
-	);
-};
-
-export { ModuleRouter };
 
 
 
